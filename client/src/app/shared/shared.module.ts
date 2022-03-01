2783ca902d143ed86d5e573351263f628c2f48cc
Hoang Trung <trungdoublelift@gmail.com>
2022-03-01 13:50:40 +0700

    merge & fix conflitct

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../components/footer/footer.component';
 import { InfHomeComponent } from '../components/inf-home/inf-home.component';
-import { NavbarComponent } from '../components/navbar/navbar.component';
+
 import {MatFormFieldModule} from '@angular/material/form-field'
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
 @NgModule({
   declarations: [
   FooterComponent,
   InfHomeComponent,
-  NavbarComponent,
   SidebarComponent,
 ],
   imports: [
@@ -19,7 +18,6 @@ import { SidebarComponent } from '../components/sidebar/sidebar.component';
   exports: [
     FooterComponent,
     InfHomeComponent,
-    NavbarComponent,
     MatFormFieldModule,
     SidebarComponent,
   ]
