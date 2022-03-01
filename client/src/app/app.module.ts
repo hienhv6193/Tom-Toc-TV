c9b86c60d427290d287461eb5b39a54dd684f68e
Hoang Trung <trungdoublelift@gmail.com>
2022-03-01 14:34:37 +0700

    .

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FooterComponent } from './components/footer/footer.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import { InfHomeComponent } from './components/inf-home/inf-home.component';
+
 import { SharedModule } from './shared/shared.module';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 
 @NgModule({
@@ -23,12 +17,9 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule,
+
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatInputModule,
+
     SharedModule,
   ],
   providers: [],
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -5,6 +5,10 @@ import { InfHomeComponent } from '../components/inf-home/inf-home.component';
 
 import {MatFormFieldModule} from '@angular/material/form-field'
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
   FooterComponent,
@@ -13,12 +17,20 @@ import { SidebarComponent } from '../components/sidebar/sidebar.component';
 ],
   imports: [
     CommonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
   ],
   exports: [
     FooterComponent,
     InfHomeComponent,
     MatFormFieldModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
     SidebarComponent,
   ]
 })
