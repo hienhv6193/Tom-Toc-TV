854c752c071d5bc07e333cbb1191e58fc16bd4e1
ThanhThien269 <thien.mlt8601@sinhvien.hoasen.edu.vn>
2022-03-01 11:44:46 +0700

    Form login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) }, { path: 'forgetPass', loadChildren: () => import('./pages/forget-pass/forget-pass.module').then(m => m.ForgetPassModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
