78d34942996dc90293f9017829c91470875adf38
hien.hv6193 <hien.hv6193@sinhvien.hoasen.edu.vn>
2022-03-08 13:44:43 +0700

    fffff

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, 
 { path: 'signup', loadChildren: () => import('./pages/signup/signup.module').then(m => m.SignupModule) }, 
 { path: 'forgetPass', loadChildren: () => import('./pages/forget-pass/forget-pass.module').then(m => m.ForgetPassModule) },
-{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },{ path: 'tranglivestream', loadChildren: () => import('./pages/tranglivestream/tranglivestream.module').then(m => m.TranglivestreamModule) },
+{ path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+{ path: 'tranglivestream', loadChildren: () => import('./pages/tranglivestream/tranglivestream.module').then(m => m.TranglivestreamModule) },
 { path: 'gamegenre', loadChildren: () => import('./pages/gamegenre/gamegenre.module').then(m => m.GamegenreModule)}];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
