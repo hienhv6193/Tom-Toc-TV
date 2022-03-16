77bd6977009413a70ce884e55e3ed5efcff7fe73
Hoang Trung <trungdoublelift@gmail.com>
2022-03-16 23:57:29 +0700

    . fix chat overflow

--- client/src/app/pages/tranglivestream/tranglivestream.component.ts
+++ client/src/app/pages/tranglivestream/tranglivestream.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class TranglivestreamComponent implements OnInit {
 
   states = [
-    {name: 'Alabama', capital: 'Montgojkhjkjkjkjkkjkjkjkjkjkjkjkccvxbxfbvxcbcxxcxcxcvxvbbmery'},
-    {name: 'Alaska', capital: 'Juneau'},
-    {name: 'Arizona', capital: 'Phoenix'},
-    {name: 'Arkansas', capital: 'Little Rock'},
+    {name: 'Alabama', capital: 'asaaaaaasasasaqweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesasasasasasasaasssssssssssssssssssdasdasdsadsasasasasasasasasasasasasasadddddddddddddddddddddddddddddddddddaaaa'},
+    {name: 'Alaska', capital: 'Junea asda d asdas das sad asda s das das das dasdasdasdasdasd asdasd asdasd asu'},
+    {name: 'Arizona', capital: 'Phoend asdsadassad asd asdasd asd as as dasd asdasd asdasd asd as dix'},
+    {name: 'Arkansas', capital: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
     {name: 'California', capital: 'Sacramento'},
     {name: 'Colorado', capital: 'Denver'},
     {name: 'Connecticut', capital: 'Hartford'},
