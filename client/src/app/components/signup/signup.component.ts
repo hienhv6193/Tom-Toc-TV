892250367dda2c3809b0dbd94e486997b1573dab
ThanhThien269 <thien.mlt8601@sinhvien.hoasen.edu.vn>
2022-03-16 11:12:28 +0700

    service

--- client/src/app/components/signup/signup.component.ts
+++ client/src/app/components/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Auth, authState, signInAnonymously, signOut, User, GoogleAuthProvider, signInWithPopup, FacebookAuthProvider } from '@angular/fire/auth';
+import { Auth, authState } from '@angular/fire/auth';
 import { SigninComponent } from '../signin/signin.component';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -13,21 +13,8 @@ export class SignupComponent implements OnInit {
   hide=true;
   hie=true;
   user:any;
-  constructor(public dialog : MatDialog,public auth:Auth, public loginSV:AuthService) {if(auth){
-    authState(this.auth).subscribe((temp:any)=>{
-       this.user=temp;
-    console.log(this.user)
-     }); 
-    }
-    
-    }
- 
-    public login(){
-      this.loginSV.login();
-    }
-    public loginF(){
-      this.loginSV.loginF();
-    }
+  constructor(public dialog : MatDialog,public auth:AuthService) {
+  }
   ngOnInit(): void {
   }
   onCloseConfirm() {
