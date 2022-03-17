70ae62f7f10915c6f9a2c6a804c4c433e274f4ed
NguyenHoangTrung <trungdoublelift@gmail.com>
2022-03-17 13:33:18 +0700

    .

--- client/src/app/services/stream.service.ts
+++ client/src/app/services/stream.service.ts
@@ -1,60 +1,89 @@
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData, getDoc, doc, query } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  getDoc,
+  doc,
+  query,
+} from '@angular/fire/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
 import { Stream } from '../models/stream.model';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StreamService {
   public streamInfo: any = new Stream();
-  public streamList!: Array<Stream>
+  public streamList!: Array<Stream>;
   public streamLink: any;
-  constructor(public router: Router, public fs: Firestore, public AcRoute: ActivatedRoute, public http: HttpClient) {
-
-  }
-
+  constructor(
+    public router: Router,
+    public fs: Firestore,
+    public AcRoute: ActivatedRoute,
+    public http: HttpClient,
+    public auth: AuthService
+  ) {}
 
   async createStream(Name: any, Description: any, StreamKey: any, UserId: any) {
-    await this.http.post(`${environment.nodejsConfig}createStream`, {
-      data: {
-        CategoryId: "2",
-        Description: Description,
-        DisLikes: [],
-        Likes: [],
-        Messages: [{}],
-        Name: Name,
-        StreamKey: StreamKey,
-        HostId: UserId,
-
-      }
-    }).subscribe(async (data) => {
-      alert(await data)
-      // let idroom=await data;
-      // this.router.navigate([`stream/${idroom}`])
-    })
+    if (this.auth.user.uid == null) {
+      alert('Vui lòng đăng nhập');
+    } else {
+      await this.http
+        .post(`${environment.nodejsConfig}createStream`, {
+          data: {
+            CategoryId: '2',
+            Description: Description,
+            DisLikes: [],
+            Likes: [],
+            Messages: [{}],
+            Name: Name,
+            StreamKey: StreamKey,
+            HostId: UserId,
+          },
+        })
+        .subscribe(async (data) => {
+          alert(await data);
+          // let idroom=await data;
+          // this.router.navigate([`stream/${idroom}`])
+        });
+    }
   }
   getStreamsData() {
     let docRef = collection(this.fs, 'Streams');
     collectionData(docRef, { idField: 'idDoc' }).subscribe((data) => {
       this.streamList = data;
-    })
+    });
   }
   getStream(Id: any) {
     let docRef = collection(this.fs, 'Streams');
     collectionData(docRef, { idField: 'idDoc' }).subscribe((data) => {
-      data.forEach(doc => {
+      data.forEach((doc) => {
         if (doc['idDoc'] == Id) {
           this.streamInfo = doc;
-
           this.streamLink = `${environment.rtmpConnectionString}:8000/live/${doc['StreamKey']}/index.m3u8`;
-
         }
-      })
-    })
+      });
+    });
+  }
+  async addChat(Id: any, Mess: any) {
+    try {
+      return await this.http
+        .post(`${environment.nodejsConfig}addChat`, {
+          data: {
+            streamId: Id,
+            UserId: this.auth.user.uid,
+            message: Mess,
+          },
+        },{responseType:'text'})
+        .subscribe(() => {});
+    } catch (err) {
+      console.log(err);
+      return
+    }
   }
 }
